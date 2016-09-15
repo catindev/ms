ad3a8ad64e7f562ca9bf55bc463fe55fd768f3ae
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2016-09-16 01:17:29 +0600

    fix: call dates

--- server/api/calls/human-date.js
+++ server/api/calls/human-date.js
@@ -4,13 +4,13 @@ moment.locale('ru');
 function formatDateForHumans( originalDate ) {
     const callDate = moment( originalDate );
     const today = moment( new Date() );
-    return today.diff( callDate, 'days' ) <= 1
+    return today.diff( callDate, 'days' ) <= 12
         ? callDate.fromNow()
         : callDate.format( 'DD MMMM HH:mm' );
 }
 
 module.exports = function formatCallDateForHumans( call ) {
     let copy = Object.assign({}, call);
-    copy.display.date = formatDateForHumans( copy.date );
+    copy.display.date = moment( copy.date ).format( 'DD MMMM HH:mm' );
     return copy;
 };
