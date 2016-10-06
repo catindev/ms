2c590231ae923a728dd5612afa188520c739ee49
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2016-10-06 14:41:55 +0600

    Нестрогий режим форматирования номера

--- server/api/format-number.js
+++ server/api/format-number.js
@@ -1,9 +1,19 @@
-module.exports = function formatNumber( phone ) {
-    if ( !phone ) throw new Error('Введите номер');
+function error(text) {
+    throw new Error( text )
+}
+
+module.exports = function formatNumber( phone, strict = true ) {
+    if ( !phone )
+        if ( strict ) error('Введите номер')
+        else return false;
     let formatted = phone.replace(/ /g,'');
     if ( formatted[0] === '+' && formatted[1] === '7' ) formatted = formatted.replace('+7','');
     if ( formatted[0] === '8' ) formatted = formatted.replace('8','');
-    if ( !/^\d+$/.test(formatted) ) throw new Error('Номер может содержать только цифры');
-    if ( formatted.length > 11 ) throw new Error('Лишние цифры в номере');
+    if ( !/^\d+$/.test(formatted) )
+        if ( strict ) error('Номер может содержать только цифры')
+        else return false;
+    if ( formatted.length > 11 )
+        if ( strict ) error('Лишние цифры в номере')
+        else return false;
     return `+7${ formatted }`;
 };
