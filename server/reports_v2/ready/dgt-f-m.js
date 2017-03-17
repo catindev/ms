33b999cfdffc568b31786d30c2e4b2c3822e9fc0
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-03-17 15:54:54 +0600

    reports version 2, fixed

---
+++ server/reports_v2/ready/dgt-f-m.js
@@ -0,0 +1,12 @@
+module.exports = {
+    id: "dgt-f-m",
+
+    name: "DG Technology",
+
+    account: "57ef8fe5dc5a071cb7d6054e",
+
+    date: {
+        start: '2017-02-01',
+        end: '2017-03-20'
+    },
+};
