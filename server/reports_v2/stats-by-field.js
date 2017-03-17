e7c1d804b750d7bb64ba92970eab0a1bcb2f3f0f
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-03-17 22:40:59 +0600

    reports2: ya metrika

--- server/reports_v2/stats-by-field.js
+++ server/reports_v2/stats-by-field.js
@@ -54,7 +54,8 @@ module.exports = function getFieldStats( account, startDate, endDate, collection
             });
 
             data.values = _.orderBy(data.values, 'count', 'desc');
-            data.values = data.values.filter( value => value.count > 0);
+            data.values = data.values.filter( value => value.count > 1);
+            data.values.length === 0 && (data.values = false );
             return data;
         });
 
