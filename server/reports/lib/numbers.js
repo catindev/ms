a9e2617bb6a07eb263d456e2097b3655fa2a8112
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-03-20 16:50:59 +0600

    Reports: refactoring

--- server/reports/lib/numbers.js
+++ server/reports/lib/numbers.js
@@ -3,7 +3,7 @@ const { orderBy, findIndex }   = require('lodash');
 
 module.exports = function getNumbers({ Contact, Number, ObjectId }) {
 
-    const query = ({ account, date }) => Contact.find({
+    const query = ({ account, date }) => ({
         account: ObjectId(account),
         created: {
             $gte: dateToISO(date.start),
@@ -30,20 +30,18 @@ module.exports = function getNumbers({ Contact, Number, ObjectId }) {
     const reduceNumbers = (numbers, number) => {
         const indx = findIndex(numbers, { name: number });
         indx === -1
-            ? numbers.push({ name: number, count: 0 })
+            ? numbers.push({ name: number, count: 1 })
             : numbers[ indx ].count += 1
         ;
         return numbers;
     };
     const clearZero = ({ count }) => count > 0;
-
     const getBad = contacts => contacts
         .filter( isNoTarget )
         .map( numberName )
         .reduce( reduceNumbers, [])
         .filter( clearZero )
     ;
-
     const getGood = contacts => contacts
             .filter( isTarget )
             .map( numberName )
@@ -52,8 +50,8 @@ module.exports = function getNumbers({ Contact, Number, ObjectId }) {
         ;
 
     const assign = (state = {}) => contacts => Object.assign({}, state, {
-        numbers_bad: getBad( contacts ),
-        numbers_good: getGood( contacts )
+        numbers_bad: orderBy(getBad(contacts), 'count', 'desc'),
+        numbers_good: orderBy(getGood(contacts), 'count', 'desc')
     });
 
     return function( reportConfig ) {
@@ -66,32 +64,4 @@ module.exports = function getNumbers({ Contact, Number, ObjectId }) {
             ;
         }
     }
-};
-
-
-const old = ({ account, date }) => data => {
-    if (data.no_target.length === 0 )
-        return Object.assign({}, data, { numbers_bad: false });
-
-    let numberz;
-    return Number.find({ account: _h.strToOID(account) })
-        .then( numbers => {
-            numberz = numbers;
-            const pipeline = numbers.map( number => Contact.find({
-                number,
-                created: { $gte: _h.dateToISO(date.start), $lt: _h.dateToISO(date.end) },
-                noTargetReason: { $exists: true },
-                name: { $exists: true }
-            }).count());
-
-            return Promise.all( pipeline );
-        })
-        .then( contacts => Object.assign({}, data, {
-            numbers_bad: _.orderBy(
-                ( numberz.map(
-                    (number, index) => ({ name: number.name, count: contacts[index] })
-                ) ).filter( number => number.count > 0 )
-                , 'count', 'desc'
-            )
-        }) );
 };
