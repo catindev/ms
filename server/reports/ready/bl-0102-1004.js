9d947c5f7bb387203e0a655e46430944af2fcbbf
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-04-11 22:22:36 +0600

    SC report

---
+++ server/reports/ready/bl-0102-1004.js
@@ -0,0 +1,12 @@
+module.exports = {
+    id: "bl-0102-1004",
+
+    name: "BodyLine",
+
+    account: "58205d3b627521466be3a09e",
+
+    date: {
+        start: '2017-02-01',
+        end: '2017-04-10'
+    }
+};
