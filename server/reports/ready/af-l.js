dc0338b0e1a768b6e6bdf7f76fb46d66492f6f2b
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-04-18 10:37:11 +0600

    AF reports

---
+++ server/reports/ready/af-l.js
@@ -0,0 +1,12 @@
+module.exports = {
+    id: "af-l",
+
+    name: "Aerofit-Ladys",
+
+    account: "588a125ca0619941426d7951",
+
+		date: {
+				start: '2017-03-01',
+				end: '2017-03-31'
+		},
+};
---
+++ server/reports/ready/af-v2.js
@@ -0,0 +1,12 @@
+module.exports = {
+    id: "af-v2",
+
+    name: "Aerofit-Восток2",
+
+    account: "588a095da0619941426d7945",
+
+		date: {
+				start: '2017-03-01',
+				end: '2017-03-31'
+		},
+};
