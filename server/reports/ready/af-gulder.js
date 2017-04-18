aa6ace7393fc00d35b0cbc8a5c1c524b89e2980a
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-04-18 10:48:37 +0600

    AF reports

--- server/reports/ready/af-gulder.js
+++ server/reports/ready/af-gulder.js
@@ -1,12 +1,12 @@
 module.exports = {
-    id: "aerofit-j-f",
+    id: "af-g",
 
     name: "Aerofit-Гульдер",
 
     account: "581d85701610b54c7dc21a00",
 
     date: {
-        start: '2017-01-01',
-        end: '2017-02-01'
+        start: '2017-03-01',
+        end: '2017-03-31'
     },
 };
