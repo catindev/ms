5de5e3fe4b4f4e3d6121858c1a13422ce9348fa9
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-04-21 13:00:57 +0600

    3d reports

---
+++ server/reports/ready/3d-akt-week.js
@@ -0,0 +1,12 @@
+module.exports = {
+	id: "3d-akt",
+
+	name: "Тридэ, Актау",
+
+	account: "58c749a85e1afb3fbe8314b3",
+
+	date: {
+		start: '2017-04-13',
+		end: '2017-04-20'
+	},
+};
---
+++ server/reports/ready/3d-atr-week.js
@@ -0,0 +1,12 @@
+module.exports = {
+	id: "3d-atr",
+
+	name: "Тридэ, Атырау",
+
+	account: "58c74a635e1afb3fbe8314bd",
+
+	date: {
+		start: '2017-04-13',
+		end: '2017-04-20'
+	},
+};
