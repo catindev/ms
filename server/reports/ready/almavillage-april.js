d0dc659b3b624b625c08e397e449c556d28b214c
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-04-26 06:33:10 +0600

    AV april

---
+++ server/reports/ready/almavillage-april.js
@@ -0,0 +1,12 @@
+module.exports = {
+		id: "almavillage-april",
+
+		name: "AlmaVillage",
+
+		account: "57d9fff08ca2296e2639ca93",
+
+		date: {
+				start: '2017-04-01',
+				end: '2017-04-26'
+		},
+};
