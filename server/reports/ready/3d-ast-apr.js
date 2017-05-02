c1771da22c81932321fc0633e65546d4c6be0fef
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-05-02 15:11:55 +0600

    3D report apr

---
+++ server/reports/ready/3d-ast-apr.js
@@ -0,0 +1,12 @@
+module.exports = {
+    id: "3d-ast-apr",
+
+    name: "Тридэ, Астана",
+
+    account: "58c5aec65e1afb3fbe8313cb",
+
+    date: {
+        start: '2017-04-1',
+        end: '2017-04-30'
+    },
+};
