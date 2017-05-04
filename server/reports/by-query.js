aed73bc145158130d7a28c9156f84a66fe9a57df
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-05-04 14:24:27 +0600

    Report builder

---
+++ server/reports/by-query.js
@@ -0,0 +1,20 @@
+const _ = require("lodash");
+const calculate = require('./index');
+
+module.exports = (request, response) => {
+    const { account, start, end } = request.query;
+
+    const report = {
+        account, id: 'by-query',
+        date: { start, end }
+    };
+    const render = data => response.render('reports/', Object.assign({}, data, { _ }));
+
+    calculate(report)
+        .then(data => json ? response.json(data) : render(data))
+        .catch(error => {
+            console.log('router error', error.message)
+            // response.end(error.message)
+            return response.render('reports/error-500');
+        });
+};
