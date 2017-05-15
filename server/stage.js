70b5d8006a1aa3061e263182c892b02a55084579
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-05-15 19:38:51 +0600

    Callback button

--- server/stage.js
+++ server/stage.js
@@ -4,7 +4,7 @@ const psize = 1024 * 1024 * 1000;
 
 const mongoose = require("mongoose");
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/MindSalesCRMtest');
+mongoose.connect('mongodb://localhost/MindSalesCRM');
 
 const express = require("express");
 const compression = require("compression");
@@ -14,12 +14,12 @@ const App = express();
 
 const isAuthenticated = require("./api/is-auth");
 
+App.use(compression({ level: 9 }));
 App.set('view engine', 'ejs');
-App.use( compression({ threshold: 0 }) );
-App.use( cookieParser() );
-App.use( '/assets', express.static('assets', { maxAge: 86400 }));
-App.use( bodyParser.urlencoded({ extended: true }) );
-App.use( bodyParser.json() );
+App.use(cookieParser());
+App.use('/assets', express.static('assets', { maxAge: 86400 }));
+App.use(bodyParser.urlencoded({ extended: true }));
+App.use(bodyParser.json());
 App.disable('view cache');
 
 App.use(bodyParser.urlencoded({
@@ -33,7 +33,7 @@ App.use(bodyParser.json({
     limit: 1024 * 1024 * 10000
 }));
 
-App.use( (error, request, response, next) => response.status(500).json({
+App.use((error, request, response, next) => response.status(500).json({
     status: 500,
     message: 'internal server error',
     reason: error.message
@@ -60,6 +60,7 @@ App.post('/v1/calls', require('./api/calls/new-call-route'));
 
 /* Contacts */
 App.get('/contacts', isAuthenticated.forView, require('./api/contacts/fetch-contacts-route'));
+App.get('/contacts/:id', isAuthenticated.forView, require('./api/contacts/fetch-contact-by-id-route'));
 App.get('/is-target/:id', isAuthenticated.forView, (request, response) => {
     response.render('contacts/target-menu', {
         title: 'Какой клиент?',
@@ -68,7 +69,6 @@ App.get('/is-target/:id', isAuthenticated.forView, (request, response) => {
         user: request.user
     });
 });
-App.get('/contacts/:id', isAuthenticated.forView, require('./api/contacts/fetch-contact-by-id-route'));
 App.get('/contacts/edit/:id', isAuthenticated.forView, require('./api/contacts/edit-contact-route'));
 App.get('/contacts/no-target/:id', isAuthenticated.forView, require('./api/contacts/edit-no-target-route'));
 App.post('/contacts/:id', isAuthenticated.forAPI, require('./api/contacts/update-contact-route'));
@@ -88,10 +88,10 @@ App.get('/stats/waiting', isAuthenticated.forView, require('./api/stats/waiting-
 App.get('/accounts/new', isAuthenticated.forView, (request, response) => {
     request.user.type === 'admin'
         ? response.render('new-account/index', {
-        title: 'Новый аккаунт',
-        page: 'new-account',
-        user: request.user
-    })
+            title: 'Новый аккаунт',
+            page: 'new-account',
+            user: request.user
+        })
         : response.redirect('/');
 });
 App.post('/accounts', isAuthenticated.forAPI, (request, response) => {
@@ -103,31 +103,39 @@ App.post('/accounts', isAuthenticated.forAPI, (request, response) => {
 });
 
 App.get('/journal', (request, response) => {
-    fs.readdir(__dirname + '/journal', function(error, files){
+    fs.readdir(__dirname + '/journal', function (error, files) {
         let list = '<p>Пусто</p>';
 
-        if ( files && files.length > 0 ) {
+        if (files && files.length > 0) {
             list = '';
             files.forEach(file => {
                 const name = file.replace('.json', '');
-                list += `<p><a href="/journal/${ name }">${ name }</a></p>`;
+                list += `<p><a href="/journal/${name}">${name}</a></p>`;
             });
 
         }
         response.send(`
             <div style="font-family:'Arial','Helvetica',sans-serif;font-size:14px;padding:3% 5%;">
                 <h1>Журнал</h1>
-                ${ list }      
+                ${ list}      
             </div>
        `);
     });
 });
 App.get('/journal/:name', (request, response) => {
-    const fileStream = fs.createReadStream(`${ __dirname }/journal/${ request.params.name }.json`);
-    fileStream.on('data', data => response.write(data) );
-    fileStream.on('end', () => response.end() );
+    const fileStream = fs.createReadStream(`${__dirname}/journal/${request.params.name}.json`);
+    fileStream.on('data', data => response.write(data));
+    fileStream.on('end', () => response.end());
 });
 
 App.get('/whosyourdaddy/:login', require('./api/system/whosyourdaddy'));
+
+
+// Reports
+App.get('/rprt/:id', require('./reports/route'));
+App.get('/report/:id', require('./reports/route'));
+App.get('/ohmystats', require('./reports/by-query'));
+
+
 
 App.listen(6669);
