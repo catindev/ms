957642ba4f006aab13e6f5e648770622d9b759d5
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-09-05 11:21:10 +0600

    debug journal

--- server/api/system/save-call-data.js
+++ server/api/system/save-call-data.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 
 module.exports = function ({ name, data }) {
-    const path = `${__dirname}/../../journal/${ name.replace(/\+/, '') }.json`;
-    fs.writeFile( path, data, error => {
-        if ( error ) throw error;
+    const path = `${__dirname}/../../journal/${name.replace(/\+/, '')}.json`;
+    fs.writeFile(path, data, error => {
+        if (error) throw error;
+        console.log('Call saved to journal');
     });
 };
