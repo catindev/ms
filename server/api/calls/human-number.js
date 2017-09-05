aae9afc74f3c943d5e3eda6175dc31fd24a6d05a
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-09-05 12:57:40 +0600

    debug callback calls

--- server/api/calls/human-number.js
+++ server/api/calls/human-number.js
@@ -1,20 +1,21 @@
 
-function formatNumberForHumans( number, notAlmaty ) {
+function formatNumberForHumans(number, notAlmaty = false) {
+    if (!number) return false;
     const re = /(?:([\d]{1,}?))??(?:([\d]{1,3}?))??(?:([\d]{1,3}?))??(?:([\d]{2}))??([\d]{2})$/;
-    let formatted = (number.replace( re,
-        ( all, a, b, c, d, e ) => ( a ? a + " " : "" ) + ( b ? b + " " : "" ) + ( c ? c + "-" : "" ) + ( d ? d + "-" : "" ) + e
+    let formatted = (number.replace(re,
+        (all, a, b, c, d, e) => (a ? a + " " : "") + (b ? b + " " : "") + (c ? c + "-" : "") + (d ? d + "-" : "") + e
     )).split(' ');
 
-    if ( notAlmaty && formatted[1] === '727' ) {
+    if (notAlmaty && formatted[1] === '727') {
         formatted[1] += formatted[2][0];
         formatted[2] = formatted[2].substring(1);
     }
 
-    return `${ formatted[0] } (${ formatted[1] }) ${ formatted[2] }`
+    return `${formatted[0]} (${formatted[1]}) ${formatted[2]}`
 }
 
-module.exports = function formatCallNumberForHumans( call, notAlmaty ) {
+module.exports = function formatCallNumberForHumans(call, notAlmaty) {
     let copy = Object.assign({}, call);
-    copy.display.phone = formatNumberForHumans( copy.contact.phone, notAlmaty );
+    copy.display.phone = formatNumberForHumans(copy.contact.phone, notAlmaty);
     return copy;
 };
