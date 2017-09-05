1eba872defb2bed79e945fd7bd1ee1eef3282731
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-09-05 15:23:34 +0600

    debug callback calls

--- server/api/calls/new-call-route.js
+++ server/api/calls/new-call-route.js
@@ -12,18 +12,19 @@ module.exports = function newCallRoute(request, response) {
 
     status = parseInt(status);
 
-    crm_call_id !== false && callback({ number: callerPhoneNumber, callID: crm_call_id });
+    if (crm_call_id !== false && crm_call_id !== "false" && crm_call_id !== "") {
+        if (status === 4) {
+            console.log('Callback call for', crm_call_id, 'ignored');
+            console.log('<<<');
+            return;
+        }
+        callback({ number: callerPhoneNumber, callID: crm_call_id });
+    }
 
     saveJournal({
         name: 'звонок ' + moment().format("DDMMM в hh:mm:ss"),
         data: JSON.stringify(request.body)
     });
-
-    if (crm_call_id !== "" && status === 4) {
-        console.log('Callback call for', crm_call_id, 'ignored');
-        console.log('<<<');
-        return;
-    }
 
     if (status === 3 || status === 4) {
 
