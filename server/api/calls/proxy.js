d431b9f48db7d1f92284d401a1563be1d58a15cc
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-10-23 10:05:16 +0600

    fix proxy

--- server/api/calls/proxy.js
+++ server/api/calls/proxy.js
@@ -27,7 +27,7 @@ function proxyCall({
         isCallback: !!crm_call_id
     }
 
-    request({ url: 'http://hooks.mindsales.crm/test', method: 'POST', json: payload }, 
+    request({ url: 'http://hooks.mindsales-crm.com/test', method: 'POST', json: payload }, 
       function(error, response, body) {
         if (error) return console.log(error.message)
         console.log(response.statusCode, body)  
